899ae3dffd506fb31231e090facaf5d6b977280e
Suresh Chaudhary <hello@suresh.im>
2026-06-23 18:35:50 +0530

    refactor: scim ui

--- ui/app/workspace/scim/page.tsx
+++ ui/app/workspace/scim/page.tsx
@@ -1,12 +1,11 @@
-import { ScrollArea } from "@/components/ui/scrollArea";
 import SCIMView from "@enterprise/components/scim/scimView";
 
 export default function SCIMPage() {
 	return (
-		<ScrollArea className="no-padding-parent w-full h-[calc(100dvh-1rem)]">
-			<div className="mx-auto max-w-7xl px-4">
+		<div className="no-padding-parent flex flex-col w-full h-[calc(100dvh-1rem)]">
+			<div className="mx-auto w-full max-w-3xl grow px-4">
 				<SCIMView />
 			</div>
-		</ScrollArea>
+		</div>
 	);
 }
